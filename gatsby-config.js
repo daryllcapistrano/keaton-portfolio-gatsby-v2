d8c03022537d03aa060d2701c40e663205c76e7a
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-05-04 16:58:25 -0700

    update gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,12 +18,11 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
+				name: `keaton-rodgers-portfolio`,
+				short_name: `KRMEDIA`,
 				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
 				display: `minimal-ui`,
+				// change the icon below
 				icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
 			}
 		}
