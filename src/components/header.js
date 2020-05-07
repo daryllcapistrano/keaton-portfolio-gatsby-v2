7413a95e0943e293b9c3a3615a9aad342e289946
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-05-06 19:24:57 -0700

    minor AppBar style change

--- src/components/header.js
+++ src/components/header.js
@@ -1,41 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import { Link } from 'gatsby';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 
-const StyledAppBar = styled(AppBar)`
-	z-index: 32;
-	padding: 16px;
-	font-size: 14px;
-	line-height: 1.5;
-	color: hsla(0,0%,100%,.7);
-	background-color: #24292e;
-`;
-
-const NavLink = styled(Link)`
-	color: rgb(109, 182, 91);
-	margin: 0 0 10px;
-	padding: 10px;
-	display: flex;
-	align-items: center;
-	flex-wrap: nowrap;
-`;
-
 const Header = ({ siteTitle }) => (
 	<React.Fragment>
-		<StyledAppBar position="static" color="default" elevation={0}>
+		<AppBar position="static" color="default" elevation={0} style={{ backgroundColor: `inherit` }}>
 			<Toolbar>
 				<Typography variant="h6" color="inherit" noWrap>
 					{siteTitle}
 				</Typography>
-				<NavLink href="#">Video</NavLink>
-				<NavLink href="#">Photos</NavLink>
-				<NavLink href="#">Contact</NavLink>
+				<div>
+					<Link href="#">Video</Link>
+					<Link href="#">Photos</Link>
+					<Link href="#">Contact</Link>
+				</div>
 			</Toolbar>
-		</StyledAppBar>
+		</AppBar>
 	</React.Fragment>
 );
 
