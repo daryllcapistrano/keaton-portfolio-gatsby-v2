8e48c3bc5fdadbb397d128190f4f9b700f22a60c
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-05-09 15:10:24 -0700

    make stickyfooter

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,52 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
-const Footer = () => (
-	<Container>
-		<div style={{ textAlign: `center`, padding: `28px` }}>
-			<a
-				href="https://www.instagram.com/keatonrodgers"
-				style={{
-					color: '#000000',
-					textDecoration: `none`
-				}}
-			>
-				KR Media
-			</a>{' '}
-			© {new Date().getFullYear()}{' '}
+function Copyright() {
+	return (
+		<Typography variant="body2" color="textSecondary">
+			{'Copyright © '}
+			<Link color="inherit" href="https://www.instagram.com/keatonrodgers">
+				Keaton Rodgers
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
+}
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex',
+		flexDirection: 'column',
+		minHeight: '55vh'
+	},
+	main: {
+		marginTop: theme.spacing(8),
+		marginBottom: theme.spacing(2)
+	},
+	footer: {
+		padding: theme.spacing(3, 2),
+		textAlign: `center`,
+		marginTop: 'auto',
+		backgroundColor: `inherit`
+	}
+}));
+
+export default function StickyFooter() {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.root}>
+			<CssBaseline />
+			<footer className={classes.footer}>
+				<Container maxWidth="sm">
+					<Copyright />
+				</Container>
+			</footer>
 		</div>
-	</Container>
-);
-
-export default Footer;
+	);
+}
