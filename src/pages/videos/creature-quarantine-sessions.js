5e980b0023d9d1c49b34b49e855c5e94b6a15b52
Daryll <dcapistrano.dev@gmail.com>
2020-07-20 13:20:52 -0700

    add creature quarantine sessions page and resources

---
+++ src/pages/videos/creature-quarantine-sessions.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../../components/common/layout"
+import SEO from "../../components/seo"
+
+const CreatureQuarantineSessions = () => (
+  <Layout>
+    <SEO title="CreatureQuarantineSessions" />
+    <div>
+      <div style={{ textAlign: "center", padding: `20px` }}>
+        <Link
+          to="/"
+          style={{
+            color: "#000000",
+            padding: "10px",
+          }}
+        >
+          Back
+        </Link>
+        <Link
+          to="/videos/wornpath"
+          style={{
+            color: "#000000",
+            padding: "10px",
+          }}
+        >
+          Next
+        </Link>
+      </div>
+    </div>
+    <div
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%" /* 16:9 */,
+        paddingTop: 25,
+      }}
+    >
+      <iframe
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+        src={`https://www.youtube.com/embed/VNgghlJIKy4`}
+        frameBorder="0"
+        allowFullScreen={true}
+        title="creature quarantine sessions"
+      />
+    </div>
+  </Layout>
+)
+
+export default CreatureQuarantineSessions
